2365dc3bf1c31d5fbd47a1e5c9ddd82b1ba01bab
Denis Gurov <denis.gurov.ddi@gmail.com>
2020-01-10 11:56:13 +0200

    Laravel 5.7 CRUD App

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 5.7 CRUD Application</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container" style="padding: 35px">
+        @yield('content')
+    </div>
+</body>
+</html>
